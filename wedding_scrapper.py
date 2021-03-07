ca4cf775fba1eb88b8defaedb2331bed53e3146b
Jin <mjin1789@gmail.com>
2021-03-07 21:03:43 +0900

    article scrapping

--- wedding_scrapper.py
+++ wedding_scrapper.py
@@ -30,7 +30,7 @@ class Parsing:
                 #tbody태그도 찾아야 함
                 title = soup.find('title').get_text()
                 print(f"테이블 확인 페이지: {title} \t주소:{self.href}")
-                self.extract_raw(tables)
+                self.scrapping_raw(tables)
 
             else:
                 print(f"테이블 미확인 페이지: {self.href}")
@@ -38,27 +38,46 @@ class Parsing:
             return
 
 
-    def extract_raw(self,tables):
-        articles = []
+    def scrapping_raw(self,tables):
+        article_hrefs = []  #결혼이란 키워드를 포함하고 최근 날짜에 올라온 게시글의 링크 모음
         for table in tables:
-            tr = table.find_all("tr")
+            tr = table.find_all("tr") #테이블의 가로를 스크래핑
             for t in tr:
                 text = t.get_text()
                 if "결혼" in text:
-                    upload_time = re.search('(\d{4}).(\d\d).(\d\d)',text)
+                    upload_time = re.search('\d{4}.\d\d.\d\d',text) #데이터 형식이 숫자 4 2 2 인 것 모두
                     if upload_time:
                         now = datetime.now()
                         upload_time = parse(upload_time.group())
-                        start_date = now - relativedelta(months =+ 1)
-                        end_date = now + relativedelta(months =+ 1)
-                        if  start_date < upload_time < end_date:
-                            article_href = t.find("a")['href'] 
-                            articles.append(article_href)
-                            print(f"내용: {text} 시간:{upload_time} href:{article_href}") 
+                        start_date = now - relativedelta(months =+ 1) #게시글의 최소 날짜
+                        end_date = now + relativedelta(months =+ 1) #게시글의 최대 날짜
+                        if  start_date < upload_time < end_date: #가끔 전화번호 잘못 긁어서 범위 설정함
+                            article_href = t.find("a")['href'] #범위안에 들어오는 게시글은 링크를 추가
+                            article_hrefs.append(article_href)
+                            print(f"내용: {text} 시간:{upload_time} href:{article_href} \nsite_href:{self.href}")
                     else:
                         continue
-      
 
+        self.scrapping_artice(article_hrefs)
+        return
+
+    def scrapping_artice(self,article_hrefs):
+        scrap_impossible = []
+        for ah in article_hrefs:
+            if self.site_status(requests.get(ah,headers = self.headers)): 
+                 soup = BeautifulSoup(requests.get(ah,headers = self.headers).text, "lxml")
+            elif self.site_status(requests.get(self.href + ah,headers = self.headers)): 
+                soup = BeautifulSoup(requests.get(self.href + ah,headers = self.headers).text,"lxml")
+            else:
+                scrap_impossible.append(self.href) 
+                return
+            content = soup.find_all(class = re.compile("content"))
+            for c in content:
+                phone_num = re.search("\d{3}.\d{4}.\d{4}")
+                print(phone_num)
+
+
+        
 class Google_Api:
     def __init__(self,query,page):
         self.query = query
@@ -96,7 +115,7 @@ class Google_Api:
 
 if __name__ == "__main__":
     query = "회원 경조사"
-    page = 1
+    page = 2
     google = Google_Api(query,page)
     google.parse_data()
     print("*"  * 20)
